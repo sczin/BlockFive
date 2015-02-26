6a211ec9439893a5a966d8742dfdf59dbe63e8b9
sczin <sczinner@bayviewglen.ca>
2015-02-26 09:35:05 -0500

    Double Array Sequence

---
+++ Unit Seven/src/com/bayviewglen/dayfour/RandomTester.java
@@ -0,0 +1,14 @@
+package com.bayviewglen.dayfour;
+
+public class RandomTester {
+
+	public static void main(String[] args) {
+
+		DoubleArraySequence d1 = new DoubleArraySequence();
+		DoubleArraySequence d2 = new DoubleArraySequence(5);
+		
+		
+
+	}
+
+}
